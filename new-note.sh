37137e144eb4c48daf0eb43c5b97811f0553b7bd
stephenjamesada <ada_stephen@proton.me>
2026-09-11 10:43:12 -0500

    feat(scripts): add second argument to new-note.sh to accept a note's topic location

--- new-note.sh
+++ new-note.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-NAME="${1:?"Usage: $0 <name>"}"
+declare -A TOPICS=(
+    [bash]=2
+    [c]=2
+    [linux]=2
+    [vim]=2
+    [git]=2
+)
 
-touch "$(date +%Y%m%d)-$NAME.md"
+NAME="${1:?"error: name required"}"
+TOPIC="$2"
+
+if [[ ! ${TOPICS[$TOPIC]+_} ]]; then
+    printf "error: invalid topic: %s\n" "$TOPIC" >&2
+    exit 1
+fi
+
+touch "$TOPIC/$(date +%Y%m%d)-$NAME.md"
