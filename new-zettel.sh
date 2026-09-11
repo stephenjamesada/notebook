2171a0f1944cd6262930d62b2bd9ab8335d6d8cc
stephenjamesada <ada_stephen@proton.me>
2026-09-11 10:45:49 -0500

    feat(scripts): add zettelkasten directory location to touch line inside new-zettel.sh to make new zettels in the directory at the top level

--- new-zettel.sh
+++ new-zettel.sh
@@ -4,4 +4,5 @@ ID="$(openssl rand -hex 3)"
 DATE="$(date +%Y%m%d)"
 NAME="${1:?"Usage: $0 <name>"}"
 
-touch "$ID-$DATE-$NAME.md"
+touch "zettelkasten/$ID-$DATE-$NAME.md"
+# Use only at the top-level for now
